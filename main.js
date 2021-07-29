be7368e3cf8371ae0b7da7a955065aa92c7527b0
Gbissell93 <gregorybisselledu@gmail.com>
2021-07-29 12:46:26 -0400

    basic functions working

---
+++ main.js
@@ -0,0 +1,48 @@
+//HTML elements dog app
+const dogBtn = document.querySelector('#dog-btn')
+const dogContent = document.querySelector('#dog-stuff');
+const dogImg = document.querySelector('#dog-img');
+console.log(dogBtn)
+//event listeners
+dogBtn.addEventListener('click', getDog)
+
+//Fetch funtions
+function getDog() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then((rawData) => {
+            return rawData.json();
+        })
+        .then((data) => {
+            console.log('data',data.message)
+            dogImg.src = data.message
+    })
+}
+ 
+//HTML weather app
+const weatherBtn = document.querySelector('#weather-btn');
+const weatherInput = document.querySelector('#weather-input');
+const weatherResults = document.querySelector('#weather');
+
+//event listener
+weatherBtn.addEventListener('click', getWeather);
+
+//weather function
+function cTOf(celcius) {
+return celcius * 1.8 + 32
+}
+
+function getWeather() {
+    fetch(`https://goweather.herokuapp.com/weather/${weatherInput.value}`)
+        .then((rawData) => {
+            return rawData.json();
+        })
+        .then((data) => {
+            console.log(data)
+            weatherResults.innerHTML = 
+            `<li id="cur-weather" class="card-body">
+            <div>Current temp ${data.temperature}</div>
+            <div>current wind speed ${data.wind}</div> 
+            <div> conditions are ${data.description}</div>
+            </li>`
+        })
+}
